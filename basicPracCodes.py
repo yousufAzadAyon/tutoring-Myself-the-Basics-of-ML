bb021ebd40fa5e2a02c5850c0b62938e728e2984
yousuf azad ayon <yousufAzadAyon@users.noreply.github.com>
2019-08-02 20:48:06 +0600

    Day Two

--- basicPracCodes.py
+++ basicPracCodes.py
@@ -22,7 +22,26 @@ from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values=np.nan,strategy='mean')
 imputer.fit( X[ : , 1:3])
 X[ : , 1:3] = imputer.transform(X[ : , 1:3])
+print(X)
+#%%
+#Encoding Catagorical Datas
 
+
+from sklearn.preprocessing import OneHotEncoder , LabelEncoder
+from sklearn.compose import ColumnTransformer
+
+label_X = LabelEncoder()
+X[ : , 0] = label_X.fit_transform(X[ : , 0])
+
+label_Y = LabelEncoder()
+Y = label_Y.fit_transform(Y)
+
+# Creating Dummy Values
+
+columnTransform = ColumnTransformer([('hotEncoder' , OneHotEncoder(),[0]) ],remainder='passthrough')
+
+X = np.array(columnTransform.fit_transform(X))
 
 
 #%%
+
