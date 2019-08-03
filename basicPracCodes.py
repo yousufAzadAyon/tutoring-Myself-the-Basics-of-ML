d36d4b6c2d04983658cbe147604882b3a4c5441b
yousuf azad ayon <yousufAzadAyon@users.noreply.github.com>
2019-08-03 23:13:19 +0600

    Updated

--- basicPracCodes.py
+++ basicPracCodes.py
@@ -42,6 +42,16 @@ columnTransform = ColumnTransformer([('hotEncoder' , OneHotEncoder(),[0]) ],rema
 
 X = np.array(columnTransform.fit_transform(X))
 
+#%%
+#Spliting dataset into training and testing sets
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
 
 #%%
+#feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
 
+X_test = sc_X.fit_transform(X_test)
+X_train = sc_X.fit_transform(X_train)
